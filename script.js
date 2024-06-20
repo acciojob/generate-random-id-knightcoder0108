bcaeb021371bf24adceefa9a871fc4ed52d277d4
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-06-21 01:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
-  // write your code here
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = parseInt(prompt("Enter a number."), 10);
 alert(makeid(l));
